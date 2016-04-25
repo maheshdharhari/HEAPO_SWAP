32379be4abf3955024306f37b4e278e84bd8a432
jata <na94jun@gmail.com>
2016-04-25 17:15:09 +0900

    POS_SWAP2 first commit

---
+++ POS_SWAP2/link_swap.sh
@@ -0,0 +1,23 @@
+# unlink
+unlink ../linux-swap/mm/pos.c
+unlink ../linux-swap/mm/pos_namespace.c
+
+unlink ../linux-swap/include/linux/pos.h
+unlink ../linux-swap/include/linux/pos_namespace.h
+
+unlink ../linux-swap/mm/Makefile
+
+# link source
+ln -s	../../POS_SWAP2/pos.c			../linux-swap/mm/pos.c
+ln -s	../../POS_SWAP2/pos_namespace.c		../linux-swap/mm/pos_namespace.c
+ln -s	../../POS_SWAP2/memory.c		../linux-swap/mm/memory.c
+
+# link header
+ln -s	../../../POS_SWAP2/pos.h		../linux-swap/include/linux/pos.h
+ln -s	../../../POS_SWAP2/pos_namespace.h	../linux-swap/include/linux/pos_namespace.h
+
+# link Makefile
+ln -s	../../POS_SWAP2/Makefile		../linux-swap/mm/Makefile
+
+# CP
+#cp 	./.config				../linux-swap/.config
