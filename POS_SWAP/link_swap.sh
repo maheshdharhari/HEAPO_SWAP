71a2a57166123581ce2fcd6124e232f07da5f96c
jedisty <jedisty@hanyang.ac.kr>
2016-04-25 15:17:01 +0900

    Add link script for linux_swap

---
+++ POS_SWAP/link_swap.sh
@@ -0,0 +1,8 @@
+# unlink
+# unlink ../linux-3.15/include/linux/pos_namespace.h
+
+# link
+# ln -s	../../../POS_SWAP/pos_namespace.h	../linux-3.15/include/linux/pos_namespace.h
+
+# CP
+cp 	./.config				../linux-swap/.config
